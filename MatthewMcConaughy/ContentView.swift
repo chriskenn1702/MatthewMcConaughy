3b9a8ab776636575f35a9f3c07db71a5b9974fb7
Christopher Kennedy <kennednu@bc.edu>
2023-02-20 17:05:34 -0500

    How alright is Matt? Alright alright alright

--- MatthewMcConaughy/ContentView.swift
+++ MatthewMcConaughy/ContentView.swift
@@ -6,16 +6,50 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    @State var numOfAlright = 1
+    @State private var audioPlayer: AVAudioPlayer!
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("How is Matt?")
+                .font(.largeTitle)
+                .fontWeight(.black)
+            Image("Matt")
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(30)
+                .shadow(radius: 30)
+            Spacer()
+            Stepper("How Alright is Matt?", value: $numOfAlright, in: 1...5)
+                .font(.title2)
+            Text("\(numOfAlright)")
+                .font(.largeTitle)
+            Spacer()
+            Button("Tell Me!"){
+                playSound(soundName: "alright")
+            }
+                .buttonStyle(.bordered)
+                .tint(.gray)
+                .foregroundColor(.blue)
+            
+            
         }
         .padding()
+    }
+    func playSound(soundName: String){
+        guard let soundFile = NSDataAsset(name: soundName) else{
+            print("😧 Could not read file named \(soundName)")
+            return
+        }
+        do{
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.numberOfLoops = numOfAlright-1
+            audioPlayer.play()
+        } catch{
+            print("🤬 ERROR: \(error.localizedDescription)")
+        }
     }
 }
 
