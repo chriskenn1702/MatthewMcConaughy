a932dae2aec3e11b607efa45e085490aff01b43a
Christopher Kennedy <kennednu@bc.edu>
2023-02-20 16:44:31 -0500

    Initial Commit

---
+++ MatthewMcConaughy/MatthewMcConaughyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatthewMcConaughyApp.swift
+//  MatthewMcConaughy
+//
+//  Created by Christopher Kennedy on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct MatthewMcConaughyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
